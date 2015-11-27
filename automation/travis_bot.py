8b9c0f402143e230f66dbc590e6780e22ff8fb4b
James <james@resin.io>
2015-11-27 19:02:42 +0700

    Testing tool

---
+++ automation/travis_bot.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""
+Notes:
+    The following enviromental variables need to be set:
+    - TRAVIS_PULL_REQUEST
+    - TRAVIS_REPO_SLUG
+    - TRAVIS_BOT_GITHUB_TOKEN
+"""
+
+from __future__ import print_function
+
+import os
+import sys
+import json
+import requests
+
+GITHUB_API_URL = 'https://api.github.com'
+
+
+def comment_on_pull_request(pr_number, slug, token, comment):
+    """ Comment message on a given GitHub pull request. """
+    url = '{api_url}/repos/{slug}/issues/{number}/comments'.format(
+        api_url=GITHUB_API_URL, slug=slug, number=pr_number)
+    response = requests.post(url, data=json.dumps({'body': comment}),
+                             headers={'Authorization': 'token ' + token})
+    return response.json()
+
+
+if __name__ == '__main__':
+    PR_NUMBER = os.environ.get('TRAVIS_PULL_REQUEST')
+    REPO_SLUG = os.environ.get('TRAVIS_REPO_SLUG')
+    TOKEN = os.environ.get('TRAVIS_BOT_GITHUB_TOKEN')
+    MESSAGE = os.environ.get('TRAVIS_BOT_NO_RESULTS_MSG', None)
+
+    comment = (
+        """
+- [ ] Changelog.
+- [ ] Tests added.
+- [ ] Security.
+- [ ] Docs.
+        """)
+
+    if all([PR_NUMBER, REPO_SLUG, TOKEN]):
+        comment_on_pull_request(PR_NUMBER, REPO_SLUG, TOKEN, comment)
+    else:
+        print('Not all neccesery variables are present')
